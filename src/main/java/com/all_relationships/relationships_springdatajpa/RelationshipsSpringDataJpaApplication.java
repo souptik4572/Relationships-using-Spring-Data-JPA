f96b62f55a9dff8ea83e775a0c7d94efc8cabca5
souptik4572 <souptiksarkar4572@gmail.com>
2022-01-15 23:48:44 +0530

    initial commit

---
+++ src/main/java/com/all_relationships/relationships_springdatajpa/RelationshipsSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.all_relationships.relationships_springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipsSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelationshipsSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/all_relationships/relationships_springdatajpa/RelationshipsSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.all_relationships.relationships_springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipsSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
